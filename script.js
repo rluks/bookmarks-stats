326d8da5b4a83a984dcad0cb677aa66caff1fe8a
rluks <rluks@seznam.cz>
2017-12-02 20:54:52 +0100

    showing count in the badge, saving values in storage

--- script.js
+++ script.js
@@ -14,22 +14,32 @@ function count(){
             queue.push([url, bookmark]);
         }
 		bookmarksCount = queue.length;
-		document.querySelector("#counter").textContent = bookmarksCount;
+		displayCount();
+		storeCount();
     });
+}
+
+function displayCount(){
+	document.querySelector("#counter").textContent = bookmarksCount;
+	browser.browserAction.setBadgeText({text: bookmarksCount.toString()});
+}
+
+function storeCount(){
+	storeNote(new Date(), bookmarksCount);
 }
 
 function displayNote(timestamp, body) {
 
 	var tableRef = document.getElementById('history-table').getElementsByTagName('tbody')[0];
 	var newRow = tableRef.insertRow(tableRef.rows.length);
-	
+
 	var newCell  = newRow.insertCell(0);
 	var newText  = document.createTextNode(timestamp);
-    newCell.appendChild(newText);
-	
+  newCell.appendChild(newText);
+
 	var newCell2  = newRow.insertCell(1);
 	var newText2  = document.createTextNode(body);
-    newCell2.appendChild(newText2);
+  newCell2.appendChild(newText2);
 }
 
 /* "Main" */
@@ -37,5 +47,3 @@ count();
 
 //clearStorage();
 initializeStorage();
-storeNote(new Date(), bookmarksCount);
-//displayNote(new Date(), "Tbody");
