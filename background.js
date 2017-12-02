be2dd728439e15f7fbb847ed6e6f5b05633bb0bc
rluks <rluks@seznam.cz>
2017-12-02 12:07:42 +0100

    listener is required in background

--- background.js
+++ background.js
@@ -1,33 +1,15 @@
+//listener required
 browser.browserAction.onClicked.addListener(() => {
     browser.tabs.create({url: "/index.html"});
 });
 
-async function findDead(error, progress) {
-    const ignoredScheme = /^(place|about|javascript|data)\:/i;
-
-    browser.bookmarks.search({}).then(bookmarks => {
-        let queue = [];
-        for (const bookmark of bookmarks) {
-            const url = bookmark.url;
-            if (!url || ignoredScheme.test(url)) {
-                continue;
-            }
-
-            queue.push([url, bookmark]);
-        }
-		progress(queue.length);
-    });
+async function noop(error, progress){
+	progress(0);
 }
 
 function onMessage(message, sender, sendResponse) {
-    if (message.type == "find_dead") {
-        findDead((bookmark, error) => {
-            //browser.tabs.sendMessage(sender.tab.id, {type: "dead", bookmark, error});
-        }, (found) => {
-            browser.tabs.sendMessage(sender.tab.id, {type: "alive", found}); //msg to script.js
-        });
+    if (message.type == "noop") {
     }
-
     return true;
 }
 
