c6283879cd14610b56ccc149025f9781142eff9f
rluks <rluks@seznam.cz>
2017-12-02 12:01:38 +0100

    counting in script.js

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-function handleAlive({found}) {
+/*function handleAlive({found}) {
     document.querySelector("#live").textContent = found;
-}
+}*/
 
-function onMessage(message) {
+/*function onMessage(message) {
 	handleAlive(message);
+}*/
+
+//browser.runtime.onMessage.addListener(onMessage);
+//browser.runtime.sendMessage({type: "find_dead"});
+
+function test(){
+	const ignoredScheme = /^(place|about|javascript|data)\:/i;
+
+    browser.bookmarks.search({}).then(bookmarks => {
+        let queue = [];
+        for (const bookmark of bookmarks) {
+            const url = bookmark.url;
+            if (!url || ignoredScheme.test(url)) {
+                continue;
+            }
+
+            queue.push([url, bookmark]);
+        }
+		//progress(queue.length);
+		//console.log(queue.length);
+		document.querySelector("#live").textContent = queue.length;
+    });
 }
 
-browser.runtime.onMessage.addListener(onMessage);
-browser.runtime.sendMessage({type: "find_dead"});
+test();
 
