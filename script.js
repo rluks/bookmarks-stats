823dbb0b1d574fbbd6eaf78288ebd16e61fcdb0a
rluks <rluks@seznam.cz>
2017-12-02 11:32:19 +0100

    no dead check

--- script.js
+++ script.js
@@ -2,20 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-function handleDead({bookmark, error}) {
-	console.log("DEAD");
-}
-
 function handleAlive({id, found}) {
     document.querySelector("#live").textContent = found;
 }
 
 function onMessage(message) {
-    if (message.type === "dead") {
-        handleDead(message);
-    } else if (message.type === "alive") {
-        handleAlive(message);
-    }
+	handleAlive(message);
 }
 
 browser.runtime.onMessage.addListener(onMessage);
