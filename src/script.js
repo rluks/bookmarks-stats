f1322fbc968279bde57be235df9a069fae5753c8
rluks <rluks@seznam.cz>
2017-12-05 17:25:11 +0100

    readability (checked with atom linter-js-standard semistandard)

--- src/script.js
+++ src/script.js
@@ -1,84 +1,84 @@
 "use strict";
 
-function onError(error) {
+function onError (error) {
   console.log(error);
 }
 
 var bookmarksCountData = {};
 
-function initializeStorage() {
+function initializeStorage () {
   var gettingAllStorageItems = browser.storage.local.get(null);
   gettingAllStorageItems.then((results) => {
     var noteKeys = Object.keys(results);
     for (let noteKey of noteKeys) {
       var curValue = results[noteKey];
-      displayNote(noteKey,curValue);
+      displayNote(noteKey, curValue);
       bookmarksCountData[noteKey] = curValue;
     }
   }, onError);
 }
 
-//stupid js
-function addZero(i) {
-    if (i < 10) {
-        i = "0" + i;
-    }
-    return i;
+//  stupid js
+function addZero (i) {
+  if (i < 10) {
+    i = '0' + i;
+  }
+  return i;
 }
 
-function formatTimestamp(timestamp){
+function formatTimestamp (timestamp) {
   var dateObj = new Date(timestamp);
   var isoDate = dateObj.toISOString();
 
-  var datepart =  isoDate.split('T')[0];
+  var datepart = isoDate.split('T')[0];
   var hours = addZero(dateObj.getHours());
-  var minutes =  addZero(dateObj.getMinutes());
-  var seconds =  addZero(dateObj.getSeconds());
-  var timePart = hours + ":" + minutes + ":" + seconds;
+  var minutes = addZero(dateObj.getMinutes());
+  var seconds = addZero(dateObj.getSeconds());
+  var timePart = hours + ':' + minutes + ':' + seconds;
 
-  var tmpStr = datepart + " " + timePart;
+  var tmpStr = datepart + ' ' + timePart;
   return tmpStr;
 }
 
-function displayNote(timestamp, body) {
+function displayNote (timestamp, body) {
   var tableRef = document.getElementById('history-table').getElementsByTagName('tbody')[0];
   var newRow = tableRef.insertRow(tableRef.rows.length);
 
-  var newCell  = newRow.insertCell(0);
-  var newText  = document.createTextNode(formatTimestamp(timestamp));
+  var newCell = newRow.insertCell(0);
+  var newText = document.createTextNode(formatTimestamp(timestamp));
   newCell.appendChild(newText);
 
-  var newCell2  = newRow.insertCell(1);
-  var newText2  = document.createTextNode(body);
+  var newCell2 = newRow.insertCell(1);
+  var newText2 = document.createTextNode(body);
   newCell2.appendChild(newText2);
 }
 
-function requestClearingHistoryStorage(){
-  browser.runtime.sendMessage({type: "clear_history"});
+function requestClearingHistoryStorage () {
+  browser.runtime.sendMessage({type: 'clear_history'});
 }
 
-function clearHistoryTableHTML(){
-  var old_tbody = document.getElementById('history-table').getElementsByTagName('tbody')[0];
-  var new_tbody = document.createElement('tbody');
-  old_tbody.parentNode.replaceChild(new_tbody, old_tbody)
+function clearHistoryTableHTML () {
+  var oldTbody = document.getElementById('history-table').getElementsByTagName('tbody')[0];
+  var newTbody = document.createElement('tbody');
+  oldTbody.parentNode.replaceChild(newTbody, oldTbody);
 }
 
-document.getElementById("clear-history-btn").addEventListener("click", function(){
-    requestClearingHistoryStorage();
-    clearHistoryTableHTML();
+document.getElementById('clear-history-btn').addEventListener('click', function () {
+  requestClearingHistoryStorage();
+  clearHistoryTableHTML();
 });
 
-function getCurrentCount(){
-  browser.runtime.sendMessage({type: "get_current_count"});
+function getCurrentCount () {
+  browser.runtime.sendMessage({type: 'get_current_count'});
 }
 
-function refreshData(){
+function refreshData () {
   getCurrentCount();
   clearHistoryTableHTML();
   initializeStorage();
 }
 
-function updateCurrent(bookmarksCount){
+function updateCurrent (bookmarksCount) {
   document.querySelector('#counter').textContent = bookmarksCount;
 }
 /* -------------------------------------------------------- */
@@ -87,10 +87,10 @@ function updateCurrent(bookmarksCount){
 
 /* -------------------------------------------------------- */
 browser.runtime.onMessage.addListener((message) => {
-   if (message.type == "current_count") {
-     updateCurrent(message.bookmarksCount);
-   }
- });
+  if (message.type === 'current_count') {
+    updateCurrent(message.bookmarksCount);
+  }
+});
 
 /* -------------------------------------------------------- */
 
@@ -110,8 +110,7 @@ setTimeout(createChart, intervalSeconds * 100);
 
 /* -------------------------------------------------------- */
 
-function createChart(){
-  var myLineChart;
+function createChart () {
   var ctx = document.getElementById('myChart').getContext('2d');
 
   var minimumDatetime = bookmarksCountData[Object.keys(bookmarksCountData)[0]];
@@ -122,43 +121,44 @@ function createChart(){
       text: 'Number of bookmarks'
     },
     scales: {
-    xAxes: [{
-      type: "time",
-            time: {
-              unit: 'minutes',
-              unitStepSize: 0.5,
-              round: 'minutes',
-              tooltipFormat: "h:mm:ss a",
-              displayFormats: {
-                hour: 'MMM D, h:mm A'
-              }
-            },
-            ticks: {
-              min: minimumDatetime,
-              max: new Date(),
-            }
-    }],
-    yAxes: [{
-      ticks: {
-        min: 3500,
-        max: 5200,
-        stepSize: 500
-      }
-    }]
+      xAxes: [{
+        type: 'time',
+        time: {
+          unit: 'minutes',
+          unitStepSize: 0.5,
+          round: 'minutes',
+          tooltipFormat: 'h:mm:ss a',
+          displayFormats: {
+            hour: 'MMM D, h:mm A'
+          }
+        },
+        ticks: {
+          min: minimumDatetime,
+          max: new Date()
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 100,
+          stepSize: 1
+        }
+      }]
     }
   };
-  myLineChart = new Chart(ctx, {
-                  type: 'line',
-                  data: {
-                      labels: Object.keys(bookmarksCountData),
-                      datasets: [{
-                          data: Object.values(bookmarksCountData),
-                          label: "Count",
-                          borderColor: "#3e95cd",
-                          fill: false
-                        }
-                      ]
-                    },
-                    options: options
-                });
+
+  var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: Object.keys(bookmarksCountData),
+      datasets: [{
+        data: Object.values(bookmarksCountData),
+        label: 'Count',
+        borderColor: '#3e95cd',
+        fill: false
+      }
+      ]
+    },
+    options: options
+  });
 }
