c6ba23ba7ddb80c1ef7ebdad8e9ce795abfcfa23
rluks <rluks@seznam.cz>
2017-12-08 21:54:28 +0100

    separated storage operations

--- src/background.js
+++ src/background.js
@@ -15,33 +15,6 @@ browser.browserAction.onClicked.addListener(() => {
 
 /* -------------------------------------------------------- */
 
-/*                        STORAGE                           */
-
-/* -------------------------------------------------------- */
-
-/* generic error handler */
-function onError(error) {
-  console.log(error);
-}
-
-function storeNote(timestamp, body) {
-  var storingNote = browser.storage.local.set({ [timestamp] : body });
-  storingNote.then(() => {
-    //displayNote(timestamp,body);
-  }, onError);
-}
-
-/* clearing storage */
-function onCleared() {
-  console.log("Storage cleared, OK.");
-}
-
-function clearStorage(){
-  var clearStorage = browser.storage.local.clear();
-  clearStorage.then(onCleared, onError);
-}
-/* -------------------------------------------------------- */
-
 /*           COMMUNCIATION with CONTENT SCRIPT              */
 
 /* -------------------------------------------------------- */
---
+++ src/storage.js
@@ -0,0 +1,22 @@
+
+/* generic error handler */
+function onError(error) {
+    console.log(error);
+}
+
+function storeNote(timestamp, body) {
+    var storingNote = browser.storage.local.set({ [timestamp] : body });
+    storingNote.then(() => {
+        //displayNote(timestamp,body);
+    }, onError);
+}
+
+/* clearing storage */
+function onCleared() {
+    console.log("Storage cleared, OK.");
+}
+
+function clearStorage(){
+    var clearStorage = browser.storage.local.clear();
+    clearStorage.then(onCleared, onError);
+}
