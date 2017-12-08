fed656162020352bb33548bb8fca9a0480665a54
rluks <rluks@seznam.cz>
2017-12-08 21:57:26 +0100

    separate math function

--- src/background.js
+++ src/background.js
@@ -71,19 +71,6 @@ function onBookmarkRemoved(id, removeInfo) {
 
 /* -------------------------------------------------------- */
 
-/*                        Math                              */
-
-/* -------------------------------------------------------- */
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
-
-
-/* -------------------------------------------------------- */
-
 /*                        MAIN                              */
 
 /* -------------------------------------------------------- */
