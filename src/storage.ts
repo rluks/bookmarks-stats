f53fd43bb4decad74e1fe9700bd2a2cd8b6d2be1
rluks <rluks@seznam.cz>
2017-12-12 23:02:27 +0100

    simplify function for loading history

--- src/storage.ts
+++ src/storage.ts
@@ -23,16 +23,13 @@ function storeCount(){
     storeNote(new Date(), bookmarksCount);
 }
 
-var bookmarksCountData = {};
-
 function initializeStorage () {
+  var historyData = {};
   var gettingAllStorageItems = browser.storage.local.get(null);
   gettingAllStorageItems.then((results) => {
     var noteKeys = Object.keys(results);
     for (let noteKey of noteKeys) {
-      var curValue = results[noteKey];
-      //displayNote(noteKey, curValue);
-      bookmarksCountData[noteKey] = curValue;
+      bookmarksCountData[noteKey] = results[noteKey];
     }
   }, onStorageError);
 }
