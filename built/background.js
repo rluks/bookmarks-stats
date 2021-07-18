5f972e02f17d350534499cb7cab64bbcd574f31e
Roman <rluks@seznam.cz>
2021-07-18 09:49:55 +0200

    adding js files generated by typescript to git

---
+++ built/background.js
@@ -0,0 +1,56 @@
+"use strict";
+const DEBUG = true;
+const bounceDelay = 250; //ms
+var bookmarksCount = 0;
+/* -------------------------------------------------------- */
+/*                  Toolbar icon action                     */
+/* -------------------------------------------------------- */
+browser.browserAction.onClicked.addListener(() => {
+    generateTestingBookmarks();
+    _.debounce(countBookmarks, bounceDelay, { 'leading': true,
+        'trailing': true });
+    browser.tabs.create({ url: "/index.html" });
+});
+/* -------------------------------------------------------- */
+/*           COMMUNCIATION with CONTENT SCRIPT              */
+/* -------------------------------------------------------- */
+function onMessage(message, sender, sendResponse) {
+    if (message.type == "clear_history") {
+        clearStorage();
+        loadHistory();
+    }
+    else if (message.type == "get_current_count") {
+        browser.tabs.sendMessage(sender.tab.id, { type: "current_count", bookmarksCount });
+    }
+    else if (message.type == "download_history") {
+        browser.tabs.sendMessage(sender.tab.id, { type: "history_data", historyData });
+    }
+}
+function displayCountBadge() {
+    browser.browserAction.setBadgeText({ text: bookmarksCount.toString() });
+}
+function onBookmarkCreated(id, bookmarkInfo) {
+    countBookmarks();
+    loadHistory();
+}
+function onBookmarkRemoved(id, removeInfo) {
+    countBookmarks();
+    loadHistory();
+}
+function generateInitialZeroCount() {
+    let minuteOld = new Date();
+    minuteOld.setSeconds(minuteOld.getSeconds() - 60);
+    storeNote(minuteOld, 0);
+}
+/* -------------------------------------------------------- */
+/*                        MAIN                              */
+/* -------------------------------------------------------- */
+browser.runtime.onMessage.addListener(onMessage);
+generateFakeHistory();
+countBookmarks();
+var historyData;
+loadHistory();
+browser.bookmarks.onCreated.addListener(_.debounce(onBookmarkCreated, bounceDelay, { 'leading': true,
+    'trailing': true }));
+browser.bookmarks.onRemoved.addListener(_.debounce(onBookmarkRemoved, bounceDelay, { 'leading': true,
+    'trailing': true }));
---
+++ built/tests.js
@@ -0,0 +1,27 @@
+/* generate a single bookmark with a number in title */
+function generateTestingBookmark(number) {
+    if (!DEBUG)
+        return;
+    var createBookmark = browser.bookmarks.create({
+        title: "bookmark" + number,
+        url: "https://www.example.org"
+    });
+}
+/* generate 3 bookmarks */
+function generateTestingBookmarks() {
+    if (!DEBUG)
+        return;
+    for (var i = 0; i < 3; i++) {
+        generateTestingBookmark(i);
+    }
+}
+/* create bookmark counts history (eg for the chart to have some data to show) */
+function generateFakeHistory() {
+    if (!DEBUG)
+        return;
+    for (let i = 600; i > 0; i -= 30) {
+        let minuteOld = new Date();
+        minuteOld.setSeconds(minuteOld.getSeconds() - i);
+        storeNote(minuteOld, getRandomInt(0, 100));
+    }
+}
