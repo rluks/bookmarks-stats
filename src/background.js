c7c61a4dec6b5c9e2a58575412429b0ab5dee8cd
Roman <rluks@seznam.cz>
2021-07-21 23:46:24 +0200

    refactored message receiver background into separate module

--- src/background.js
+++ src/background.js
@@ -1,18 +1,6 @@
-import { sendBookmarkStats } from '/bg/send.js';
-import { refreshBookmarkStats } from '/bg/bookmarkdata.js';
 import { onBrowserAction } from '/bg/browseraction.js';
 import { handleRemoved } from '/bg/foregroundinfo.js';
-
-function onMessage(message, sender, sendResponse) {
-    if (message.type == "hello") {
-
-        //send what stats are available right away
-        sendBookmarkStats();
-
-        //but update the stats and then once ready send the new stats
-        refreshBookmarkStats().then(sendBookmarkStats);
-    }
-}
+import { onMessage } from '/bg/receiver.js';
 
 browser.runtime.onMessage.addListener(onMessage);
 browser.browserAction.onClicked.addListener(onBrowserAction);
---
+++ src/bg/receiver.js
@@ -0,0 +1,15 @@
+import { sendBookmarkStats } from '/bg/send.js';
+import { refreshBookmarkStats } from '/bg/bookmarkdata.js';
+
+function onMessage(message, sender, sendResponse) {
+    if (message.type == "hello") {
+
+        //send what stats are available right away
+        sendBookmarkStats();
+
+        //but update the stats and then once ready send the new stats
+        refreshBookmarkStats().then(sendBookmarkStats);
+    }
+}
+
+export {onMessage};
