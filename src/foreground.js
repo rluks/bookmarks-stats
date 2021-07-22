7df0a572c0942899e86b001de9ccb79c96dc2d63
Roman <rluks@seznam.cz>
2021-07-22 20:23:58 +0200

    UI shows current bookmark count and number of data points

--- src/foreground.js
+++ src/foreground.js
@@ -1,7 +1,16 @@
 "use strict";
 
+function getCurrentBookmarkCount(data){
+    let maxKey = Object.keys(data).reduce((a, b) => a > b ? a : b);
+    let bookmarkCount = data[maxKey];
+    console.log(maxKey + " : " + bookmarkCount);
+    return bookmarkCount;
+}
+
 function updateUI(data){
     console.log(data);
+    updateBookmarkCount(getCurrentBookmarkCount(data));
+    updateDatapointsCount(Object.keys(data).length);
 }
 
 browser.runtime.sendMessage({ type: "hello" });
