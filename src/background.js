e0104a306512ea41f2f2d60071b8bbf8fab6e903
Roman <rluks@seznam.cz>
2021-07-23 01:55:52 +0200

    maxqueue working like a charm

--- src/background.js
+++ src/background.js
@@ -10,23 +10,43 @@ browser.tabs.onRemoved.addListener(handleRemoved);
 let enableCall = true;
 const defaultInterval = 1000;
 let interval = defaultInterval;//milliseconds
+let queue = 0;
+let maxQueue = 0;
 
 function handleThrottleTimeout(){
 
-  refreshBookmarkStats().then(()=>{
+  if(queue > maxQueue){
+    console.log(new Date().toISOString() + " more requests came during timeout, waiting if there would be even more...");
+    maxQueue = queue;
+    setTimeout(handleThrottleTimeout, interval);
+    return;
+  }
+  else if(queue > 0){
+    console.log(new Date().toISOString() + " requests came during timeout, refreshing...");
+    queue = 0;
+    maxQueue = 0;
+    refreshBookmarkStats().then(()=>{
+      console.log(new Date().toISOString() + " done refreshing. Give me more.");
       enableCall = true;
-  });
-  
+    });
+
+  }
+  else{
+    console.log(new Date().toISOString() + " all peaceful, listening for more.");
+    enableCall = true;
+  }
 }
 
 function throttle() {
 
     if (!enableCall) {
+      queue++;
       return;
     }
 
     enableCall = false;
 
+    console.log(new Date().toISOString() + " refreshing and setting timeout");
     refreshBookmarkStats().then(setTimeout(handleThrottleTimeout, interval));
 }
 
