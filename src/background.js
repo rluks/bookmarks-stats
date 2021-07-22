30a496fe43136e5fe8d2a0cafa4a5c1d55060ea9
Roman <rluks@seznam.cz>
2021-07-23 01:31:18 +0200

    simplified timeout with promise ready for more, but it's calling it twice

--- src/background.js
+++ src/background.js
@@ -13,52 +13,33 @@ const maxInterval = 5000;
 const defaultInterval = 400;
 let interval = defaultInterval;//milliseconds
 
-
-/*function handleThrottleTimeout(){
-  enableCall = true;
-
-  if(interval > defaultInterval){
-    queueDepth = 0;
-    interval = interval/2;
-    if(interval < defaultInterval){
-      interval = defaultInterval;
-    }
-
-    refreshBookmarkStats();//TODO no chaining,  
-
-    if(interval > defaultInterval){
-      setTimeout(handleThrottleTimeout, interval);//TODO remove calling itself
-    }
-  }
-}*/
-
 function handleThrottleTimeout(){
-  enableCall = true;
 
-  if(interval > defaultInterval){
-    queueDepth = 0;
-    interval = interval/2;
-    if(interval < defaultInterval){
+  refreshBookmarkStats().then(()=>{
       interval = defaultInterval;
-    }
-
-    refreshBookmarkStats().then(interval = defaultInterval);
-  }
+      queueDepth = 0;
+      console.log("ready for more");
+      
+      enableCall = true;
+  });
+  
 }
 
 function throttle() {
 
     if (!enableCall) {
-      //console.log(new Date() + " throttle()");
       queueDepth++;
       interval = 2*interval;
       if(interval > maxInterval){
         interval = maxInterval;
       }
+      //console.log("throttle()" + new Date().toISOString() + " " + interval + " queuedepth: " + queueDepth);
       return;
     }
 
     enableCall = false;
+    //console.log("throttle()" + new Date().toISOString() + " " + interval + " queuedepth: " + queueDepth);
+
     refreshBookmarkStats().then(setTimeout(handleThrottleTimeout, interval));
 }
 
