fc45b089cfb58cbe3ad24f59dc6594be2e264d28
Roman <rluks@seznam.cz>
2021-07-22 21:01:39 +0200

    removed unused on import code

--- src/background.js
+++ src/background.js
@@ -16,26 +16,6 @@ function addBookmarkListeners() {
     browser.bookmarks.onRemoved.addListener(onBookmarkChange);
 }
 
-/*
-
-/As of 2021 07 - not yet supported in FF, see https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/bookmarks/onImportBegan
-
-function onImportStart(){
-    browser.bookmarks.onCreated.removeListener(onBookmarkChange);
-    browser.bookmarks.onRemoved.removeListener(onBookmarkChange);
-}
-
-function onImportEnd(){
-    addBookmarkListeners();
-    refreshBookmarkStats();
-}
-
-/
-//browser.bookmarks.onImportBegan.addListener(onImportStart); 
-//browser.bookmarks.onImportEnded.addListener(onImportEnd);
-
-*/
-
 addBookmarkListeners()
 
 refreshBookmarkStats();
