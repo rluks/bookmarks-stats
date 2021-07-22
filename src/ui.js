3938dd5c18c55a209ad193b37fa93ce803d0888f
Roman <rluks@seznam.cz>
2021-07-22 22:07:12 +0200

    ui tweaks

--- src/ui.js
+++ src/ui.js
@@ -20,9 +20,9 @@ function updateBookmarkCount(bookmarksCount) {
 
 function updateDatapointsCount(count) {
     if(count === 0){
-        document.querySelector('#datapointscount').textContent = "No stats history yet. Continue using your bookmarks as usual and check back later.";
+        document.querySelector('#datapointscount').textContent = "No bookmark history. Continue using your bookmarks as usual and check back later.";
     }else{
-        document.querySelector('#datapointscount').textContent = "Stats history: " + count + " changes to bookmarks recorded.";
+        document.querySelector('#datapointscount').textContent = "Bookmarks history: " + count + " changes recorded.";
     }
 }
 
