b43d4d9f106d095a4206f64b61398eab0a3e2075
Roman <rluks@seznam.cz>
2021-07-23 21:25:29 +0200

    removed unused code

--- src/ui-chart.js
+++ src/ui-chart.js
@@ -1,12 +1,5 @@
 import {getMinDate, getMaxDate} from '/ui-count.js';
 
-/* find minimum and maximum in array */
-function arrMin(array) { return Math.min.apply(Math, array); }
-function arrMax(array) { return Math.max.apply(Math, array); }
-
-/* rounding */
-function toInt(n) { return Math.round(Number(n)); }
-
 function createCanvas() {
     var canvasDiv = document.getElementById('chart');
     while(canvasDiv.firstChild){
