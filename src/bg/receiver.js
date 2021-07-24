697b39c4da81cc203307a35969eb85507759824e
Roman <rluks@seznam.cz>
2021-07-24 20:43:53 +0200

    forgot about import, and don't refresh on hello message because of double chart refresh

--- src/bg/receiver.js
+++ src/bg/receiver.js
@@ -1,14 +1,9 @@
 import { sendBookmarkStats } from '/bg/send.js';
-import { refreshBookmarkStats } from '/bg/bookmarkdata.js';
+import { clearHistory } from '/bg/bookmarkdata.js';
 
 function onMessage(message, sender, sendResponse) {
     if (message.type == "hello") {
-
-        //send what stats are available right away
         sendBookmarkStats();
-
-        //but update the stats and then once ready send the new stats
-        refreshBookmarkStats().then(sendBookmarkStats);
     }else if (message.type == "clear-history"){
         clearHistory();
     }
