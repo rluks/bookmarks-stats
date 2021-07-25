fc3c3fdbc781b1a1ac57466f742f9b400b24fb71
Roman <rluks@seznam.cz>
2021-07-25 17:13:55 +0200

    comments

--- bg/throttle.js
+++ bg/throttle.js
@@ -9,23 +9,23 @@ let maxQueue = 0;
 function handleThrottleTimeout(){
 
   if(queue > maxQueue){
-    //more requests came during timeout, waiting if there would be even more...");
+    //more requests came during timeout, waiting if there would be even more
     maxQueue = queue;
     setTimeout(handleThrottleTimeout, interval*0.66);
     return;
   }
   else if(queue > 0){
-    //requests came during timeout, refreshing...");
+    //requests came during timeout, refreshing
     queue = 0;
     maxQueue = 0;
     refreshBookmarkStats().then(()=>{
-      //done refreshing. Give me more.");
+      //done refreshing. Give me more
       enableCall = true;
     });
 
   }
   else{
-    //no more requests, listening for more.");
+    //no more requests, listening for more
     enableCall = true;
   }
 }
@@ -39,7 +39,7 @@ function throttle() {
 
     enableCall = false;
 
-    //refreshing and setting timeout");
+    //refreshing and setting timeout
     refreshBookmarkStats().then(setTimeout(handleThrottleTimeout, interval));
 }
 
